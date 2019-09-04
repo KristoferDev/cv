9b483aec6c49fc589009519f142d1b818a2083d1
Kristofer <kristofer.stensson@gmail.com>
2019-09-04 23:23:18 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import 'Zone.js';
-
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { LanguageComponent } from './components/language/language.component';
+
+const appRoutes: Routes = [
+  { path: 'Home', component: ProfileComponent },
+  { path: 'Knowledge', component: LanguageComponent }
+];
 
 @NgModule({
   declarations: [
@@ -15,7 +22,12 @@ import { LanguageComponent } from './components/language/language.component';
     ProfileComponent,
     LanguageComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
